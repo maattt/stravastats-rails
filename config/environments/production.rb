8097fa732692b31bd02b0ddcef48b4a2c1ae9e09
Matthieu Hébert <me@matthieuhebert.fr>
2020-11-11 18:40:34 +0100

    production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,3 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+
+REDIRECT_URI = "https://guarded-reaches-85296.herokuapp.com/auth"
